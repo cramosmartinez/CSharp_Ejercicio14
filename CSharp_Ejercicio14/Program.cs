3b058dabf9c13cfbf4fb04bf0203b1e74a97f413
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 18:30:02 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio14/Program.cs
@@ -0,0 +1,10 @@
+﻿//Crear un programa calcule cuantas cifras tiene un número entero positivo (pista: se puede hacer dividiendo varias veces entre 10).
+Console.WriteLine("Ingrese un número entero positivo:");
+int numero126 = Convert.ToInt32(Console.ReadLine());
+int contador = 0;
+while (numero126 > 0)
+{
+    numero126 /= 10;
+    contador++;
+}
+Console.WriteLine("El número ingresado tiene " + contador + " cifras.");
